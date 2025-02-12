a075931d5ce5a6dd3ad2b0bd4a397f468f6529a8
stan-2021131 <sergiocoromac@gmail.com>
2025-02-12 17:32:40 -0600

    Se puse código para la funcionalidad del generador de números

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        String file = "List.txt";
+        FileControl fc = new FileControl();
+        String result = fc.writeList(file, fc.generateNumbers(10, 0, 10));
+        System.out.println(result);
     }
 }
