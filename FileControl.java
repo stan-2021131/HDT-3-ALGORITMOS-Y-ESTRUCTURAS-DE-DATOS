732fd6d93f25229a336089bab895c8a8cc09d843
stan-2021131 <sergiocoromac@gmail.com>
2025-02-12 13:02:35 -0600

    Se agregaron funciones para el manjero de archivos y generación de números

--- FileControl.java
+++ FileControl.java
@@ -1,3 +1,56 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class FileControl {
-    
+
+    //Generación de enteros, necesita la cantidad y el rango de los números
+    public Integer[] generateNumbers(int cant, int min, int max){
+        Integer[] list = new Integer[cant];
+        for(int i = 0; i<cant;i++){
+            int randNum = ThreadLocalRandom.current().nextInt(min, max+1);
+            list[i] = randNum;
+        }
+        return list;
+    }
+
+    //Trae la lista de enteros desde el txt
+    @SuppressWarnings("null")
+    public Integer[] loadList(String fileName){
+        Integer[] list = null;
+        int listCount = 0;
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            if((line = bufferedReader.readLine()) != null){
+                String[] data = line.split(" ");
+                for (String data1 : data) {
+                    //Conversión de los datos de String a Integer
+                    if (data1.matches("\\d+")) {
+                        list[listCount] = Integer.valueOf(data1);
+                        listCount += 1;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error reading line: " + e.getMessage());
+        }
+        return list;
+    }
+
+    //Escritura de la lista de enteros en el txt
+    public String writeList(String fileName, Integer[] list){
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            //Convierte la lista de Integers a una linea de texto, separando con espacios los valores
+            String line = Arrays.stream(list).map(String::valueOf).reduce((a, b) -> a + " " + b).orElse("");
+            bufferedWriter.write(line);
+            return "The list is added to txt";
+        } catch (Exception e) {
+            System.out.println("Error writing line: " + e.getMessage());
+        }
+        return "Error adding list";
+    }
 }
